d9082f9b3ee6c4a15df515162d2d884b692c64bb
AnthonyLbt <148220286+AnthonyLbt@users.noreply.github.com>
2023-10-17 15:35:27 +0200

    Add files via upload

---
+++ vue/vue_activitesRecentes.php
@@ -0,0 +1,54 @@
+<?php 
+    include "vue_entete.php";
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Activités récentes | Employeur</title>
+    <link rel="stylesheet" href="css/activites_recentes.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="js/bootstrap.bundle.js">
+  </head>
+
+  <body>
+<br>
+
+      <h1 class="titreprincipal"><b>Activités récentes</b></h1> 
+
+      <div class="rounded-3 col-md-8 row flex-nowrap bg-black bg-opacity-50 w-75 mx-auto p-3 align-self-center menu-pleine-hauteur">
+
+
+        <div class="col-md-6">
+          <h1 class="text-center text-white">Candidature spontanée</h1>
+         <div class="col">
+           <hr>
+ 
+        <div class="rounded-3 col-md-12 row flex-nowrap w-75 mx-auto p-3 align-self-center text-white">Prénom / Nom</div>
+        <div class="rounded-3 col-md-12 row flex-nowrap w-75 mx-auto p-3 align-self-center text-white">Texte style lettre de motivation</div>
+
+          </div>
+        </div>
+
+<div class="vr border-3 border border-dark rounded-4 p-0"></div>
+
+        <div class="col-md-6">
+          <h1 class="text-center text-white">Réponse à une offre</h1>
+          <hr>
+
+          <div class="rounded-3 col-md-12 row flex-nowrap w-75 mx-auto p-3 align-self-center text-white">Caissier H/F E.Leclerc</div>
+          <div class="rounded-3 col-md-12 row flex-nowrap w-75 mx-auto p-3 align-self-center text-white">Prénom / Nom</div>
+          <div class="text-center">
+          <a href="#" class="btn btn-outline-light btn btn-block">Accéder</a>
+          </div>
+        </div>
+
+      </div>
+
+
+
+  </body>
+
+</html>
---
+++ vue/vue_mesOffres.php
@@ -0,0 +1,63 @@
+<?php 
+    include "vue_entete.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mes offres | Employeur</title>
+    <link rel="stylesheet" href="css/mes_offres.css">
+</head>
+<body>
+    
+
+    <h1 class="titreprincipal"><b>Mes offres</b></h1>
+
+    <div class="cards">   
+        <div class="card bg-dark text-white rounded border-dark border-3">
+            <img src="./images/image_test.jpg" alt="Image test">
+
+            <div class="card-body">
+                <h2>Caissier H/F E.Leclerc</h2>
+                <p>Secteur : <u>Commerce</u></p>
+                <p class ="description_offre">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>                
+                <a href="#" class="btn btn-outline-light btn-lg btn-block">Modifier</a>
+                <a href="#" class="btn btn-danger btn-lg btn-block">Supprimer</a>
+            </div>
+        </div>
+
+        <div class="card bg-dark text-white rounded border-dark border-3">
+            <img src="./images/image_test.jpg" alt="Image test">
+        
+            <div class="card-body">
+                <h2>Assistant H/F de récolte</h2>
+                <p>Secteur : <u>Agroalimentaire</u></p>
+                <p class="description_offre">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                <a href="#" class="btn btn-outline-light btn-lg btn-block">Modifier</a>
+                <a href="#" class="btn btn-danger btn-lg btn-block">Supprimer</a>
+            </div>
+        </div>
+        
+        
+
+        <div class="card bg-dark text-white rounded border-dark border-3">
+            <img src="./images/image_test.jpg" alt="Image test">
+            
+            <div class="card-body">
+                <h2>Vendeur H/F Lidl</h2>
+                <p>Secteur : <u>Commerce</u></p>
+                <p class ="description_offre">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+                <a href="#" class="btn btn-outline-light btn-lg btn-block">Modifier</a>
+                <a href="#" class="btn btn-danger btn-lg btn-block">Supprimer</a>
+            </div>
+        </div>
+    </div>          
+
+      
+        
+</body>
+</html>
