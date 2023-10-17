489b8ea8c9e10c9516f056693b0117496425b66d
Maël <65314603+Yosh64@users.noreply.github.com>
2023-10-17 16:19:25 +0200

    Ajout index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+if (!isset($_GET['section']) OR $_GET['section'] == 'index')
+{
+    include_once('controleur/portail.php');
+}
+
+else
+{
+	if ($_GET['section'] == 'login')
+	{  
+			include_once('controleur/login.php');
+	}
+	elseif ($_GET['section'] == 'dashboardEtud')
+	{  
+			include_once('controleur/dashboardEtudiant.php');
+	}
+	elseif ($_GET['section'] == 'dashboardEmploi'){
+
+			include_once('controleur/dashboardEmployeur.php');
+
+	}elseif ($_GET['section'] == 'depotAnno'){
+
+			include_once('controleur/depotAnnonces.php');
+	}else{
+		header('location: index.php');
+	}
+}
+?>
