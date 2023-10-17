ef55e8aebc6f536360bc45b5d48460bfaad3a2a2
gbnrt <146883786+gbnrt@users.noreply.github.com>
2023-10-17 16:53:53 +0200

    test commit

--- index.php
+++ index.php
@@ -3,15 +3,15 @@ if (!isset($_GET['section']) OR $_GET['section'] == 'index')
 {
     include_once('controleur/portail.php');
 }
-
+// commenatires
 else
 {
 	if ($_GET['section'] == 'login')
-	{  
+	{
 			include_once('controleur/login.php');
 	}
 	elseif ($_GET['section'] == 'dashboardEtud')
-	{  
+	{
 			include_once('controleur/dashboardEtudiant.php');
 	}
 	elseif ($_GET['section'] == 'dashboardEmploi'){
