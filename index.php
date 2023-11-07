3f937f0c8f21702f21a527888a287d1416c6a680
AnthonyLbt <148220286+AnthonyLbt@users.noreply.github.com>
2023-11-07 17:46:22 +0100

    index

--- index.php
+++ index.php
@@ -1,28 +1,11 @@
 <?php
-if (!isset($_GET['section']) OR $_GET['section'] == 'index')
-{
-    include_once('controleur/portail.php');
-}
-// commenatires
-else
-{
-	if ($_GET['section'] == 'login')
-	{
-			include_once('controleur/login.php');
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
 	}
-	elseif ($_GET['section'] == 'dashboardEtud')
-	{
-			include_once('controleur/dashboardEtudiant.php');
-	}
-	elseif ($_GET['section'] == 'dashboardEmploi'){
-
-			include_once('controleur/dashboardEmployeur.php');
-
-	}elseif ($_GET['section'] == 'depotAnno'){
-
-			include_once('controleur/depotAnnonces.php');
-	}else{
-		header('location: index.php');
-	}
-}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/dashboard/');
+	exit;
 ?>
+Something is wrong with the XAMPP installation :-(
