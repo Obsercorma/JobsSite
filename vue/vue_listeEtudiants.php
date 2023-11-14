fe2b4e4b282f139f07fb8c8129eb7997660ce9bd
gbnrt <146883786+gbnrt@users.noreply.github.com>
2023-11-14 17:53:16 +0100

    bootstrap des pages

--- vue/vue_listeEtudiants.php
+++ vue/vue_listeEtudiants.php
@@ -1,38 +1,50 @@
-<?php 
+<?php
     include "vue/vue_entete.php";
 ?>
     <h1 class="title fw-bold">Liste étudiants</h1>
+
+
     <section class="list-students">
-        <div class="card text-center fw-bold w-25 h-100">
+
+
+      <div class="cards">
+
+      <div class="row justify-content-around">
+        <div class="card text-center fw-bold col-3 border-3 border-dark">
             <div class="card-body d-flex flex-column justify-content-center align-items-center">
-                <img src="./defaultStudent.png" alt="Etudiant" class="img-student">
+                <img src="vue/images/user.png" alt="Etudiant" class="img-student w-75">
                 <h3 class="student-name">Nom Prénom</h3>
             </div>
             <div class="card-footer">
                 <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                <a href="#" class="btn btn-outline-dark fw-bold border-2">Consulter le profil</a>
+                <a href="#" class="btn btn-primary fw-bold border-2">Consulter le profil</a>
             </div>
         </div>
-        <div class="card text-center fw-bold w-25 h-100">
+
+        <div class="card text-center fw-bold col-3 border-3 border-dark">
             <div class="card-body d-flex flex-column justify-content-center align-items-center">
-                <img src="./defaultStudent.png" alt="Etudiant" class="img-student">
+                <img src="vue/images/user.png" alt="Etudiant" class="img-student w-75">
                 <h3 class="student-name">Nom Prénom</h3>
             </div>
             <div class="card-footer">
                 <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                <a href="#" class="btn btn-outline-dark fw-bold border-2">Consulter le profil</a>
+                <a href="#" class="btn btn-primary fw-bold border-2">Consulter le profil</a>
             </div>
         </div>
-        <div class="card text-center fw-bold w-25 h-100">
+
+        <div class="card text-center fw-bold col-3 border-3 border-dark">
             <div class="card-body d-flex flex-column justify-content-center align-items-center">
-                <img src="./defaultStudent.png" alt="Etudiant" class="img-student">
+                <img src="vue/images/user.png" alt="Etudiant" class="img-student w-75">
                 <h3 class="student-name">Nom Prénom</h3>
             </div>
             <div class="card-footer">
                 <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                <a href="#" class="btn btn-outline-dark fw-bold border-2">Consulter le profil</a>
+                <a href="#" class="btn btn-primary fw-bold border-2">Consulter le profil</a>
             </div>
         </div>
+      </div>
+
+    </div>
     </section>
     <div></div>
 <?php include_once("vue/vue_footer.php"); ?>
