0cf4ce31906ec63f276c0d914165f331cb23ef69
AnthonyLbt <148220286+AnthonyLbt@users.noreply.github.com>
2023-11-21 17:09:14 +0100

    Depot CV avec la preview

--- vue/vue_depotCV.php
+++ vue/vue_depotCV.php
@@ -1,15 +1,33 @@
 <?php 
     include "vue/vue_entete.php";
 ?>
-
 <body>
     <h1 class="title text-white">Déposer CV</h1>
-  <div class="mb-3">
-  <input class="form-control" type="file" id="formFile">
-</div>
-<embed src= 
-"https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210101201653/PDF.pdf#toolbar=0"> 
-    <a href="index.php?section=lesOffres" class="btn btn-light mb-3">Découvrir les offres</a>
-    
+    <div class="mb-3">
+    <input class="form-control" type="file" id="formFile" accept=".png, .jpg, .jpeg, .pdf">
+    </div>
+    <div id="pdfPreview"></div>
+    <a href="index.php?section=lesOffres" class="btn btn-light mb-3" style="display: inline-block; width: auto;">Découvrir les offres</a>
+
+    <script>
+        document.getElementById('formFile').addEventListener('change', function(event) {
+            var file = event.target.files[0];
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                var preview = document.getElementById('pdfPreview');
+                if (file.type === "application/pdf" || file.type === "image/jpeg" || file.type === "image/png") {
+                    if (file.type === "application/pdf") {
+                        preview.innerHTML = '<embed src="' + e.target.result + '" type="application/pdf" width="100%" height="600px" />';
+                    } else {
+                        preview.innerHTML = '<img src="' + e.target.result + '" alt="Aperçu de l\'image" style="max-width: 100%; max-height: 600px;" />';
+                    }
+                } else {
+                    alert('Seuls les fichiers PDF, PNG et JPG sont pris en charge.');
+                }
+            };
+            reader.readAsDataURL(file);
+        });
+
+    </script>
 </body>
 </html>
