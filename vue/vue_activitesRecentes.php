e4b9fb20e85dd4ab60c0cd6c73121dfd4c25acb5
TechRusse <techrusse@mythologicarte.fr>
2023-12-11 11:12:50 +0100

    Update vue_activitesRecentes.php

--- vue/vue_activitesRecentes.php
+++ vue/vue_activitesRecentes.php
@@ -38,8 +38,8 @@
               ?>
 
               <div class="bg-primary bg-opacity-25 rounded-3 border border-2 border-primary my-2">
-                <div class="rounded-3 col-md-12 row w-75 mx-auto p-3 align-self-center text-white"><?= $intitOffre ?></div>
-                <div class="rounded-3 col-md-12 row w-75 mx-auto p-3 align-self-center text-white"><?= $prenom, " ", $nom ?></div>
+                <h3 class="rounded-3 col-md-12 row w-75 mx-auto p-3 align-self-center text-white">Job: <?= $intitOffre ?></h3>
+                <p class="rounded-3 col-md-12 row w-75 mx-auto p-3 align-self-center text-white fw-bold">Employeur: <?= $prenom, " ", $nom ?></p>
                   <div class="d-grid d-md-block text-center">
                       <a href="#" class="btn btn-success mb-3 mx-2">Accepter</a>
                       <a href="#" class="btn btn-outline-danger mb-3 mx-2">Refuser</a>
